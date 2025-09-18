827738c0cef61008e55d9e7ee2bc929a45ef2b8f
Nurdiyar <nurdi.instagramm@gmail.com>
2025-09-18 19:07:44 +0500

    test sql connection and add maven dependency

---
+++ target/test-classes/file.sql
@@ -0,0 +1,10 @@
+# CREATE TABLE users(
+#     user_id INT PRIMARY KEY AUTO_INCREMENT,
+#     name VARCHAR(30),
+#     age INT,
+#     email VARCHAR(70)
+# );
+
+# INSERT INTO users(name, age, email) VALUES ('steve', 55, 'steve@gmail,com');
+
+# SELECT * FROM users;
